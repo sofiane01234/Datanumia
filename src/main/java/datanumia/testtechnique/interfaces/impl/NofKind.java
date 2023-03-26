4faf173853a239f6febc423d0f13b0ddc48fd14c
Sofiane <cs_chenna@esi.dz>
2023-03-26 19:37:34 +0200

    Test Technique : intial commit

--- src/main/java/datanumia/testtechnique/interfaces/impl/NofKind.java
+++ src/main/java/datanumia/testtechnique/interfaces/impl/NofKind.java
@@ -6,16 +6,16 @@ import java.util.List;
 
 public class NofKind implements YatzyStrategy {
 
-    private int diceValue;
+    private int diceWithSameValue;
 
-    public NofKind(int diceValue) {
-        this.diceValue = diceValue;
+    public NofKind(int diceWithSameValue) {
+        this.diceWithSameValue = diceWithSameValue;
     }
 
     @Override
     public int calculateScore(DiceRoll diceRoll) {
-        List<Integer> highestOccurenceEqualOrGreaterThen = diceRoll.gettheHighestOccurenceEqualOrGreaterThen(diceValue);
-        return highestOccurenceEqualOrGreaterThen.isEmpty() ? 0 : highestOccurenceEqualOrGreaterThen.get(0) * diceValue;
+        List<Integer> highestOccurenceEqualOrGreaterThen = diceRoll.gettheHighestOccurenceEqualOrGreaterThen(diceWithSameValue);
+        return highestOccurenceEqualOrGreaterThen.isEmpty() ? 0 : highestOccurenceEqualOrGreaterThen.get(0) * diceWithSameValue;
 
     }
 }
